2e35503aff5ce6a222579ad6dc3688296ca7aedf
Forrester <jason.forrester@accenture.com>
2024-05-17 19:52:09 -0500

    practical project

---
+++ app.js
@@ -0,0 +1,37 @@
+//Fizz Buzz = part one
+
+//for (let num = 1; num <= 100; num++) {
+    //if (num % 3 === 0 && num % 5 === 0) {
+      //  console.log("Fizz Buzz");
+   // } else if (num % 3 === 0) {
+   //     console.log("Fizz");
+  //  } else if (num % 5 === 0) {
+  //      console.log("Buzz");
+  //  } else {
+  //      console.log(num);
+  // }
+//}
+
+//Prime Time = part two
+
+//function isPrime(n){
+  //  if (n <= 1) return false 
+    //for (let i = 2;i < n; i++ ){
+      //  if (n % i === 0){
+        //    return false;
+          //  return true;
+        //}
+    //}
+//}
+//console.log('Is $(n) a prime number?',isPrime(n));
+
+//Feeling loopy= part 3
+//function parseCSV(csvString) {
+  //  let rows = csvString.split("\n");
+    //for (let i = 0; i < rows.length; i++) {
+      //  let cells = rows[i].split(",");
+        //console.log(cells[0], cells[1], cells[2], cells[3]);
+    //}
+//}
+//let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+//parseCSV(csvData);
