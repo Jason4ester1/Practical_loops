493ffe4ceced40de5f66c8589268f293c992af3e
Forrester <jason.forrester@accenture.com>
2024-05-22 13:15:17 -0500

    last-commit

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ function isPrime(n){
 console.log('Is $(n) a prime number?',isPrime(n));
 
 //Feeling loopy= part 3
-//function parseCSV(csvString) {
-  //  let rows = csvString.split("\n");
-    //for (let i = 0; i < rows.length; i++) {
-      //  let cells = rows[i].split(",");
-        //console.log(cells[0], cells[1], cells[2], cells[3]);
-    //}
-//}
-//let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-//parseCSV(csvData);
+function parseCSV(csvString) {
+   let rows = csvString.split("\n");
+    for (let i = 0; i < rows.length; i++) {
+       let cells = rows[i].split(",");
+        console.log(cells[0], cells[1], cells[2], cells[3]);
+    }
+}
+let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+parseCSV(csvData);
